1273f94e2e3a69bb895801928597877e6a94745c
God <god@god.com>
2018-01-27 11:38:09 +0800

    fix some erroer

--- 所有文章/txt2md.py
+++ 所有文章/txt2md.py
@@ -1,10 +1,14 @@
+# -*- coding: utf-8 -*-
+# Convert "所有文章-按时间.txt" to "所有文章-按时间.md". 
+# 2018-01-27
+
 import os
 import re
 py_location = os.path.dirname(os.path.abspath(__file__))
 print(py_location) # 打印当前 py 文件所在目录
 
-os.chdir(py_location)
-print(os.getcwd())
+os.chdir(py_location) # 更改工作目录
+print(os.getcwd()) # 打印工作目录
 
 f = open(r'所有文章-按时间.txt', 'r', encoding='utf-8')
 
@@ -15,4 +19,5 @@ f.close()
 f = open(r'所有文章-按时间.md', 'w', encoding='utf-8')
 f.write(lines)
 f.close()
-print('done!')
+
+print('Conversion Completed!')
